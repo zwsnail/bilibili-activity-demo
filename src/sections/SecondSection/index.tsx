ee9e6f6c7a588298b1c100d2669771564082abc2
coco <coco@192.168.0.102>
2022-12-02 22:02:07 +0800

    完成第二部分样式

--- src/sections/SecondSection/index.tsx
+++ src/sections/SecondSection/index.tsx
@@ -1,9 +1,40 @@
 import { FC } from "react";
-
+import styles from "./styles.module.scss";
+import CartoonImage from "../../assets/cartoon.jpg";
+import MovieImage from "../../assets/movie.png";
+import LifeImage from "../../assets/life.jpg";
+import FoodImage from "../../assets/food.jpg";
 const SecondSection: FC = () => {
     return (
-        <div>
+        <div className={styles.secondSection}>
             <h1>SecondSection</h1>
+            {/* Tabs */}
+            <ul>
+                <li>动画</li>
+                <li>美食</li>
+                <li>电影</li>
+                <li>生活</li>
+            </ul>
+
+            {/* Tabs */}
+            <div>
+                <section>
+                    <h2>动画</h2>
+                    <img src={CartoonImage} alt="Cartoon" />
+                </section>
+                <section>
+                    <h2>美食</h2>
+                    <img src={FoodImage} alt="Food" />
+                </section>
+                <section>
+                    <h2>电影</h2>
+                    <img src={MovieImage} alt="Movie" />
+                </section>
+                <section>
+                    <h2>生活</h2>
+                    <img src={LifeImage} alt="Life" />
+                </section>
+            </div>
         </div>
     )
 }
