580412cbf30403a9a191f8ecae35c75abc19ba0a
coco <coco@192.168.0.102>
2022-12-04 22:44:59 +0800

    update first section with swiper

--- src/sections/FirstSection/index.tsx
+++ src/sections/FirstSection/index.tsx
@@ -1,13 +1,43 @@
 import { FC } from "react";
-import BannerImage from "../../assets/banner.jpg";
 import styles from "./styles.module.scss";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+import { Navigation, Pagination, Mousewheel, Keyboard } from "swiper";
+
+// import BannerImage from "../../assets/banner.jpg";
+import BannerImage1 from "../../assets/banner1.png";
+import BannerImage2 from "../../assets/banner2.png";
+import BannerImage3 from "../../assets/banner3.png";
+import BannerImage4 from "../../assets/banner4.png";
+
+
 
 const FirstSection: FC = () => {
     return (
-        <div className="styles.firstSection">
-            <h1>First Section</h1>
-            <img src={BannerImage} alt="Banner" />
+        <div className={styles.firstSection}>
+            <Swiper
+                cssMode={true}
+                navigation={true}
+                pagination={true}
+                mousewheel={true}
+                keyboard={true}
+                modules={[Navigation, Pagination, Mousewheel, Keyboard]}
+                className="mySwiper"
+            >
+                {/* <SwiperSlide><img src={BannerImage} alt="Banner" /></SwiperSlide> */}
+                <SwiperSlide><img src={BannerImage1} alt="Banner1" /></SwiperSlide>
+                <SwiperSlide><img src={BannerImage2} alt="Banner2" /></SwiperSlide>
+                <SwiperSlide><img src={BannerImage3} alt="Banner3" /></SwiperSlide>
+                <SwiperSlide><img src={BannerImage4} alt="Banner4" /></SwiperSlide>
+
+            </Swiper>
         </div>
+
+
     )
 }
 
